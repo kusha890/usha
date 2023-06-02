21e67740a8b4a3c1de29319a2dae125ec4aae52d
kusha890 <131345491+kusha890@users.noreply.github.com>
2023-06-02 17:55:22 +0530

    Add files via upload

---
+++ Program207.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class Program207
+{
+public static void main(String[]args)
+{char ch;
+System.out.println("Enter a ASCII Character :");
+Scanner sc=new Scanner(System.in);
+ch=sc.next().charAt(0);
+int x=(int)ch;
+System.out.println("The value of "+ ch+" is"+ x);
+}
+}
+
---
+++ Program208.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class Program208
+{
+public static void main(String[]args)
+{char ch;
+Scanner sc=new Scanner(System.in);
+ch=sc.next().charAt(0);
+int ascii_var=(int)ch;
+System.out.println("ASCII of "+ch+"is"+ascii_var);
+}
+}
+
